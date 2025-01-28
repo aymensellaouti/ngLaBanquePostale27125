32be42c26b34d135aa083d6777e00cddf4df9f27
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-28 15:00:51 +0100

    exemple route param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CvComponent } from './cv/cv/cv.component';
 import { TodoComponent } from './todo/todo/todo.component';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { ColorComponent } from './components/color/color.component';
+import { SecondComponent } from './components/second/second.component';
 
 const routes: Routes = [
   { path: '', component: FirstComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'todo', component: TodoComponent},
   { path: 'word', component: MiniWordComponent},
   { path: 'color', component: ColorComponent},
+  // j'accepte toutes routes avec un segment
+  { path: ':quelqueChose', component: SecondComponent},
 ];
 
 @NgModule({
--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -7,4 +8,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   `,
   styles: [``],
 })
-export class SecondComponent {}
+export class SecondComponent {
+  acr = inject(ActivatedRoute);
+}
