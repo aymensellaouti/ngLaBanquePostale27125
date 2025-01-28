858f78d8934387eb5ccb9acef244e9b312ff03c3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-28 12:22:04 +0100

    todoWeek

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Cv } from '../model/cv';
 import { LoggerService } from 'src/app/services/logger.service';
 import { SayHelloService } from 'src/app/services/say-hello.service';
+import { TodoService } from 'src/app/todo/service/todo.service';
 
 @Component({
   selector: 'app-cv',
@@ -76,6 +77,7 @@ export class CvComponent {
       18
     ),
   ];
+  todoService = inject(TodoService);
   // sayHelloService = new SayHelloService();
   constructor(
     private loggerService: LoggerService,
--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { TodoService } from "../service/todo.service";
+import { Todo } from "../model/todo";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-
+  providers: [TodoService]
 })
 export class TodoComponent {
+  todoService = inject(TodoService);
+  todos: Todo[] = this.todoService.getTodos();
+  todo = new Todo();
   constructor() {}
+
+  addTodo() {
+    this.todoService.addTodo(this.todo);
+    this.todo = new Todo();
+  }
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
