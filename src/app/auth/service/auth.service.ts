fc4ac078e24343e215241bb51b3d30e67f5ee774
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-29 13:54:57 +0100

    helpers auth

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,20 +1,27 @@
 import { inject, Injectable } from '@angular/core';
 import { Credentials } from '../dto/credentials.dto';
 import { HttpClient } from '@angular/common/http';
+import { APP_CONST } from 'src/app/config/app-constantes.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
   http = inject(HttpClient);
 
-  constructor() {
+  constructor() {}
 
+  login(credentials: Credentials) {}
+
+  setToken(token: string): void {
+    localStorage.setItem(APP_CONST.tokenName, token);
   }
 
-  login(credentials: Credentials) {
-
+  getToken(): string {
+    return localStorage.getItem(APP_CONST.tokenName) ?? '';
+  }
+  removeToken(): void {
+    localStorage.removeItem(APP_CONST.tokenName);
   }
 }
 
