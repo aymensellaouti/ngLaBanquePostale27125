3c41ce44d08c2bf89c5698d190ab2db8a3669e50
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-29 16:37:18 +0100

    update details

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -4,37 +4,45 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CvService } from "../services/cv.service";
 import { APP_ROUTES } from "src/app/config/app-routes.config";
 import { AuthService } from "src/app/auth/service/auth.service";
+import { catchError, EMPTY, Observable } from "rxjs";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 
 @Component({
-    selector: 'app-details-cv',
-    templateUrl: './details-cv.component.html',
-    styleUrls: ['./details-cv.component.css'],
-    standalone: true,
-    imports: [],
+  selector: 'app-details-cv',
+  templateUrl: './details-cv.component.html',
+  styleUrls: ['./details-cv.component.css'],
+  standalone: true,
+  imports: [],
 })
 export class DetailsCvComponent {
-  cv: Cv | null = null;
-  acr = inject(ActivatedRoute);
   cvService = inject(CvService);
-  router = inject(Router);
+  acr = inject(ActivatedRoute);
   authService = inject(AuthService);
+  router = inject(Router);
+  id = this.acr.snapshot.params['id'];
+  cv$: Observable<Cv> = this.cvService.getCvById(this.id).pipe(
+    catchError((e) => {
+      this.router.navigate([APP_ROUTES.cv]);
+      return EMPTY;
+    })
+  );
+  cv = toSignal(this.cv$);
 
   constructor() {
-    const id = this.acr.snapshot.params['id'];
-    this.cvService.getCvById(id).subscribe({
-      next: (cv) => {
-        this.cv = cv;
-      },
-      error: (e) => {
-        this.router.navigate([APP_ROUTES.cv]);
-      },
-    });
+
+    // .subscribe({
+    //   next: (cv) => {
+    //     this.cv = cv;
+    //   },
+    //   error: (e) => {
+    //     this.router.navigate([APP_ROUTES.cv]);
+    //   },
+    // });
   }
 
-  deleteCv() {
-    if (this.cv) {
-      this.cvService.deleteCvById(this.cv.id).subscribe({
+  deleteCv(id: number) {
+      this.cvService.deleteCvById(id).subscribe({
         next: () => {
           this.router.navigate([APP_ROUTES.cv]);
         },
@@ -43,5 +51,4 @@ export class DetailsCvComponent {
         },
       });
     }
-  }
 }
